4b028c2bb4595b76e425dcc2c811fd833ea69dee
queckezz <fabian.eichenberger@gmail.com>
2016-07-20 09:50:50 +0200

    add test for key argument

--- test/index.js
+++ test/index.js
@@ -11,13 +11,24 @@ var test = require('tape')
 
 test('should work', function (t) {
   t.deepEqual(mapValues(add1, {a: 1, b: 2, c: 3}), [2, 3, 4])
+  t.end()
+})
 
+test('takes a key', function (t) {
+  t.deepEqual(
+    mapValues(join, {keep: 'calm', and: 'test on'}),
+    ['keep calm', 'and test on']
+  )
   t.end()
 })
 
 /**
  * Helpers
  */
+
+function join (val, key) {
+  return key + ' ' + val
+}
 
 function add1 (n) {
   return n + 1
